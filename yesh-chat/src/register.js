ab0ebb09c5bcbfab60f6a4e4ce221872fa20b6b5
shiracohen4 <shira.co1@gmail.com>
2023-05-06 20:10:28 +0300

    Added mandatory form input fields

--- yesh-chat/src/register.js
+++ yesh-chat/src/register.js
@@ -4,24 +4,24 @@ function Reg() {
     return (
         <>
             <img id="logo" src="logo.png" alt="logo"></img>
-            <form action="a.php" method="get">
+            <form action="./" method="get">
                 <div class="card border-dark" id="login">
                     <div class="card-header text-center">
                         <h5>register</h5>
                     </div>
                     <div class="card-body" id="reg-card-body">
                         <div id="content">
-                            <h5 class="card-title">Username: <input></input></h5>
-                            <h5 class="card-text">Password: <input></input></h5>
-                            <h5 class="card-text">Confirm password: <input></input></h5>
-                            <h5 class="card-text">Display name: <input></input></h5>
-                            <h5 class="card-text">Picture: <input type="file" id="upload_imj"></input></h5>
-                            <a href="./" id="register" class="btn custom-btn">Register</a>
+                            <h5 class="card-title">Username: <input type="text" name="username" required></input></h5>
+                            <h5 class="card-text">Password: <input type="password" name="password" required></input></h5>
+                            <h5 class="card-text">Confirm password: <input type="password" name="confirmPassword" required></input></h5>
+                            <h5 class="card-text">Display name: <input type="text" name="displayName" required></input></h5>
+                            <h5 class="card-text">Picture: <input type="file" name="picture" id="upload_imj" required></input></h5>
+                            <button type="submit" id="register" class="btn custom-btn">Register</button>
                         </div>
                         <div class="card-footer text-center text-body-secondary">
                             <span>
                                 Already registered?
-                                <a href="./">Click here</a>
+                                <a href="./"> Click here </a>
                                 to login
                             </span>
                         </div>
